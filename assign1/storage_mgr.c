fe2d1bbc1532902a144624193c57cb2dff0953e1
Mark <mlgameng@gmail.com>
2022-09-17 15:40:03 -0500

    optimizing

--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -154,14 +154,11 @@ RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
     if (fHandle -> curPagePos == pageNum){ // already in correct pos, ex. curpage 0 and pageNum 0
         //fwrite(memPage, 1, PAGE_SIZE, file);
         // have to go to correct pos, start of pageNum
-        fseek(file, pageNum * PAGE_SIZE, SEEK_SET);
         fwrite(memPage, sizeof(char), PAGE_SIZE, file);
-        fHandle -> mgmtInfo = file;
     }else{
         // have to go to correct pos, start of pageNum
         fseek(file, pageNum * PAGE_SIZE, SEEK_SET);
         fwrite(memPage, sizeof(char), PAGE_SIZE / sizeof(char), file);
-        fHandle -> mgmtInfo = file;
     }
     fHandle -> curPagePos = pageNum + 1; // read block from pos 0, so 0 -> 1, so now pointer starting at page 1. +1
 
