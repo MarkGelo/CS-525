c19aca9b43aa352784df1c51d549a96cf7f09955
Mark <mlgameng@gmail.com>
2022-09-17 01:24:13 -0500

    nice?

--- assign1/README.txt
+++ assign1/README.txt
@@ -1,6 +1,6 @@
 
 
-Functions:
+File Related Methods:
     initStorageManager: Not really sure what this function is supposed to do. Just made it say "Initialized Storage Manager"
 
     createPageFile: Checks if file already exists, if it does returns -1. Don't want to overwrite an existing file. Initial file size is 1 page and filled with '\0' bytes.
@@ -11,6 +11,20 @@ Functions:
 
     destroyPageFile: Delete the file. Not given the fhandle, so can't delete fhandle associated with the file. Though, probably customary to do closePageFile then destroyPageFile. If couldn't remove, throws RC_FILE_NOT_FOUND.
 
+Read and Write Methods:
+    readBlock: First checks if the block at pageNum is possible to be read. If not, then throw RC_READ_NON_EXISTING_PAGE. Then checks if current page pos is same as pageNum if so, just start reading the block and store the content to memPage. If its not the same page, then seek to the start of pageNum block and then read. At the end, update the curPagePos, but incrementing 1.
+
+    getBlockPos: Just return curPagePos from the file handle
+
+    readFirstBlock, readLastBlock: Just uses the readBlock method, and pageNum is 0 or fHandle -> totalNumPages - 1. -1 cuz pageNum needs to start at 0
+
+    readPreviousBlock, readCurrentBlock, readNextBlock: Just uses the readBlock method, but uses fHandle -> curPagePos or getBlockPos and then either add 0, 1, or minus 1
+
+    writeBlock, writeCurrentBlock:
+
+    appendEmptyBlock:
+
+    ensureCapacity:
 
 Not sure why "Hint: You should reserve some space in the beginning of a file to store information such as the total number of pages." when total number of pages is already in the file handle.
 
--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -50,9 +50,7 @@ RC openPageFile (char *fileName, SM_FileHandle *fHandle){
     fseek(file, 0L, SEEK_END); // seek to end of file, get pos
     int size = ftell(file); // long int
     fHandle -> totalNumPages = size / PAGE_SIZE; // calc num of pages
-    /*
-        Calculation of # pages should be correct cuz should always be in PAGE_SIZE blocks anyways. If don't need whole block, just fill with \0 to reach PAGE_SIZE? right?
-    */
+    //Calculation of # pages should be correct cuz should always be in PAGE_SIZE blocks anyways. If don't need whole block, just fill with \0 to reach PAGE_SIZE? right?
     //printf("Size: %d\n", size);
     //printf("Total Num Pages: %d\n", fHandle -> totalNumPages); // checking if calculation is right
     rewind(file); // do i need to do this, since i saved the fp before doing the seek
@@ -63,7 +61,7 @@ RC openPageFile (char *fileName, SM_FileHandle *fHandle){
 }
 
 /*
-close page file -- close, should i remove fhandle?
+close page file -- close, should i also remove fhandle?
 */
 RC closePageFile (SM_FileHandle *fHandle){
     FILE *file = fHandle -> mgmtInfo;
@@ -75,9 +73,134 @@ RC closePageFile (SM_FileHandle *fHandle){
     return RC_OK;
 }
 
+/*
+When doing destroyPageFile, do closePageFile first, so it closes and also removes fhandle
+*/
 RC destroyPageFile (char *fileName){
     if (remove(fileName) != 0){
         return RC_FILE_NOT_FOUND;
     }
+    return RC_OK;
+}
+
+// Read and Write Methods
+
+/*
+read block at pos pageNum from the file using fhandle, and then store that page content to memPage
+if file has less than the pageNum page, then return RC_READ_NON_EXISTING_PAGE
+Page starts at 0
+*/
+RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    // pageNum starts at 0
+    if (fHandle -> totalNumPages - 1 < pageNum){ // need to be false to cont ... total 1, pageNum 0 -> 0 < 0 FALSE CORRECT, total 1 pageNum 1 -> 0 < 1 TRUE ERROR
+        return RC_READ_NON_EXISTING_PAGE;
+    }
+
+    FILE *file = fHandle -> mgmtInfo;
+    if (fHandle -> curPagePos == pageNum){ // already in correct pos, ex. curpage 0 and pageNum 0
+        fread(memPage, 1, PAGE_SIZE, file);
+    }else{
+        // have to go to correct pos, start of pageNum
+        fseek(file, pageNum * PAGE_SIZE, SEEK_SET);
+        fread(memPage, 1, PAGE_SIZE, file);
+    }
+    fHandle -> curPagePos = pageNum + 1; // read block from pos 0, so 0 -> 1, so now pointer starting at page 1. +1
+
+    return RC_OK;
+}
+
+/*
+why is this needed when you can just do fHandle -> curPagePos
+*/
+int getBlockPos (SM_FileHandle *fHandle){
+    return fHandle -> curPagePos;
+}
+
+/*
+The following read blocks are all similar, and can just use the readBlock method
+uses fHandle -> curPagePos or getBlockPos cuz they same thing
+*/
+RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return readBlock(0, fHandle, memPage);
+}
+
+RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return readBlock(fHandle -> curPagePos - 1, fHandle, memPage);
+}
+
+RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return readBlock(getBlockPos(fHandle), fHandle, memPage); // just using getBlockPos and fHandle -> curPagePos, interchangeably lmao. should work
+}
+
+RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return readBlock(getBlockPos(fHandle) + 1, fHandle, memPage);
+}
+
+RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return readBlock(fHandle -> totalNumPages - 1, fHandle, memPage); //pageNum starts at 0
+}
+
+/*
+write block to page file according to pagenum, basically same as readblock but write
+*/
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    // pageNum starts at 0
+    if (fHandle -> totalNumPages - 1 < pageNum){ // need to be false to cont ... total 1, pageNum 0 -> 0 < 0 FALSE CORRECT, total 1 pageNum 1 -> 0 < 1 TRUE ERROR
+        return RC_WRITE_FAILED;
+    }
+
+    // same thing as readblock but write
+    FILE *file = fHandle -> mgmtInfo;
+    if (fHandle -> curPagePos == pageNum){ // already in correct pos, ex. curpage 0 and pageNum 0
+        fwrite(memPage, 1, PAGE_SIZE, file);
+    }else{
+        // have to go to correct pos, start of pageNum
+        fseek(file, pageNum * PAGE_SIZE, SEEK_SET);
+        fwrite(memPage, 1, PAGE_SIZE, file);
+    }
+    fHandle -> curPagePos = pageNum + 1; // read block from pos 0, so 0 -> 1, so now pointer starting at page 1. +1
+
+    return RC_OK;
+}
+
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    return writeBlock(getBlockPos(fHandle), fHandle, memPage);
+}
+
+/*
+append an empty block at end of file - similar to createpage, just gotta make sure to do it at the end of file
+*/
+RC appendEmptyBlock (SM_FileHandle *fHandle){
+    FILE *file = fHandle -> mgmtInfo; // create file
+    int curBlock = getBlockPos(fHandle);
+    
+    if (curBlock == fHandle -> totalNumPages){ // already at end -- 1 total page, beginning is curBlock 0, curBlock 1 means at end, so
+        char *p = malloc(PAGE_SIZE);
+        memset(p, '\0', PAGE_SIZE);
+        fwrite(p, 1, PAGE_SIZE, file);
+        free(p);
+    }else{ // seek to end
+        fseek(file, 0L, SEEK_END);
+        char *p = malloc(PAGE_SIZE);
+        memset(p, '\0', PAGE_SIZE);
+        fwrite(p, 1, PAGE_SIZE, file);
+        free(p);
+    }
+    fHandle -> totalNumPages += 1;
+
+    return RC_OK;
+}
+
+/*
+if file has less pages than specified, increase
+*/
+RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
+    if (fHandle -> totalNumPages < numberOfPages){
+        int i;
+        for (i = 0; i < numberOfPages - fhandle -> totalNumPages; i++){
+            appendEmptyBlock(fHandle);
+        }
+    }
+
     return RC_OK;
 }
