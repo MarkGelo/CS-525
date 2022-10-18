e7f4e8aea99f9cd0f47b353fd96ae9f03daf9fd1
Mark <mlgameng@gmail.com>
2022-10-17 19:02:55 -0500

    init

---
+++ assign3/dberror.h
@@ -0,0 +1,64 @@
+#ifndef DBERROR_H
+#define DBERROR_H
+
+#include "stdio.h"
+
+/* module wide constants */
+#define PAGE_SIZE 4096
+
+/* return code definitions */
+typedef int RC;
+
+#define RC_OK 0
+#define RC_FILE_NOT_FOUND 1
+#define RC_FILE_HANDLE_NOT_INIT 2
+#define RC_WRITE_FAILED 3
+#define RC_READ_NON_EXISTING_PAGE 4
+
+// error codes for assign2
+#define RC_PAGE_NOT_FOUND 100 // when looking for pages to markdirty and such, if not found
+#define RC_NO_PAGE_CAN_BE_EVICTED 101 // when looking to evict, but nothing can be evicted
+#define RC_UNABLE_TO_PIN 102 // when pinning but something goes awry
+#define RC_STRATEGY_NOT_IMPLEMENTED 103
+#define RC_BUFFER_POOL_SHUTDOWN_ERROR 104
+
+#define RC_RM_COMPARE_VALUE_OF_DIFFERENT_DATATYPE 200
+#define RC_RM_EXPR_RESULT_IS_NOT_BOOLEAN 201
+#define RC_RM_BOOLEAN_EXPR_ARG_IS_NOT_BOOLEAN 202
+#define RC_RM_NO_MORE_TUPLES 203
+#define RC_RM_NO_PRINT_FOR_DATATYPE 204
+#define RC_RM_UNKOWN_DATATYPE 205
+
+#define RC_IM_KEY_NOT_FOUND 300
+#define RC_IM_KEY_ALREADY_EXISTS 301
+#define RC_IM_N_TO_LAGE 302
+#define RC_IM_NO_MORE_ENTRIES 303
+
+/* holder for error messages */
+extern char *RC_message;
+
+/* print a message to standard out describing the error */
+extern void printError (RC error);
+extern char *errorMessage (RC error);
+
+#define THROW(rc,message) \
+		do {			  \
+			RC_message=message;	  \
+			return rc;		  \
+		} while (0)		  \
+
+// check the return code and exit if it is an error
+#define CHECK(code)							\
+		do {									\
+			int rc_internal = (code);						\
+			if (rc_internal != RC_OK)						\
+			{									\
+				char *message = errorMessage(rc_internal);			\
+				printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, message); \
+				free(message);							\
+				exit(1);							\
+			}									\
+		} while(0);
+
+
+#endif
